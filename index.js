05a6c41fcba3546e4c37b5e03dcf6b6e4cc90550
Larry D. Lee jr <larry_lee@elucidsolutions.com>
2016-06-23 12:54:31 -0400

    Revised the index.html. Removed the back to top button. Revised the styling.

--- index.js
+++ index.js
@@ -5,24 +5,4 @@
 */
 
 (function () {
-  // I. Display/hide the Back to Top tab.
-  setInterval (main_displayBackToTop, 1000);
-
-  // II. Set the Back to Top tab's click event handler.
-  $('#back_to_top').click (
-    function (event) {
-      event.preventDefault ();
-      $('html, body').animate ({
-        scrollTop: $('#top').offset ().top
-      });
-  });
 }) ();
-
-// This function hides/displays the Back to Top tab.
-function main_displayBackToTop () {
-  if ($(window).scrollTop() > 200) {
-    $('#back_to_top').animate ({opacity: 1});
-  } else {
-    $('#back_to_top').animate ({opacity: 0});
-  }
-}
